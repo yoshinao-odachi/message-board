b70523563b02f2f0667c5d2af73462c34633c73e
yoshinao odachi <yoshinao-odachi@united.jp>
2016-09-06 11:12:34 +0000

    何かやってる

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  def index
+  def create
     @message = Message.new
     # Messageを全て習得する。
     @messages = Message.all
@@ -7,8 +7,14 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      # メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
   end
   
   private
